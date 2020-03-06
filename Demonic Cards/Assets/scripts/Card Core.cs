d2b200e068b0bebdcce03df92f11e55c947d5a9c
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-03-05 21:17:09 -0500

    yeet

--- Demonic Cards/Assets/scripts/Card Core.cs
+++ Demonic Cards/Assets/scripts/Card Core.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card
+public abstract class Card
 {
     //5 x2 weapon cards, 2 x5 armor cards, 5 class cards, 5 backstory cards
     //nts: make all cards singletons
+
+    public abstract TileSet getTileSet();
+    public abstract void use(Being user, Tile target);
+}
+
+public class TileSet{
+
 }
---
+++ Demonic Cards/Assets/scripts/Room_Core.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+   private List<List<Tile>> tiles = new List<List<Tile>>();
+   private int xSize, ySize;
+
+    public Room(int xSize2,int ySize2){
+        xSize = xSize2;
+        ySize = ySize2;
+        for (int i = 0; i < xSize; i++)
+        {
+            tiles.Add(new List<Tile>());
+            for (int j = 0; j < ySize; j++)
+            {
+                tiles[i-1].Add(new Tile());
+            }
+        }
+    }
+
+    public Tile getTile(int x, int y){
+        try
+        {
+            return tiles[x][y];
+        }
+        catch (System.Exception)
+        {
+            return null;
+        }
+    }
+}
+
+public class Tile{
+    public Being occupant = null;
+    public Floor floor;
+    public Surface surf = Surface.NONE;
+}
+
+public class Floor{
+    //sprite and stuff
+}
+
+public enum Surface{
+    NONE, FIRE, ACID, LAVA, POISON
+}
