a53666ab8898dd7a645bcebe0812d5940c77e0fd
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-03-08 15:45:21 -0400

    Update select_brain.cs

--- Demonic Cards/Assets/scripts/selector/select_brain.cs
+++ Demonic Cards/Assets/scripts/selector/select_brain.cs
@@ -9,7 +9,8 @@ public class select_brain : MonoBehaviour
     public TMP_Dropdown weaponOne, weaponTwo, headArmor,chestArmor,armArmor,legArmor,feetArmor,classHolder,backstoryHolder;
     private int personId = 0;
     private string wSel1, wSel2, hSel, cSel,aSel,lSel,fSel;
-    private List<string> weapons = new List<string>();
+    private List<string> weapons = new List<string>(),headGear = new List<string>(),chestGear = new List<string>()
+    ,armGear = new List<string>(),legGear = new List<string>(),feetGear = new List<string>();
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +30,22 @@ public class select_brain : MonoBehaviour
     void selectWeapon2(){
         wSel2 = weapons[weaponTwo.value];
     }
+    void selectHead(){
+        hSel = headGear[headArmor.value];
+    }
+    void selectChest(){
+        cSel = chestGear[chestArmor.value];
+    }
+    void selectArm(){
+        aSel = armGear[armArmor.value];
+    }
+    void selectLeg(){
+        lSel = legGear[legArmor.value];
+    }
+    void selectFeet(){
+        fSel = feetGear[feetArmor.value];
+    }
+
 
     public void nextPerson(){
         if (personId > 0){
@@ -42,9 +59,74 @@ public class select_brain : MonoBehaviour
             //remove stuff from lists
             weapons.Remove(wSel1);
             if (!wSel1.Equals(wSel2)){
-                weapons.Remove(wSel2)
+                weapons.Remove(wSel2);
             }
             //load stuff into dropdowns
+            List<TMP_Dropdown.OptionData> list = new List<TMP_Dropdown.OptionData>();
+            foreach (string s in weapons)
+            {
+                TMP_Dropdown.OptionData d = new TMP_Dropdown.OptionData();
+                d.text = s;
+                list.Add(d);
+                
+            }
+            weaponOne.AddOptions(list);
+            weaponTwo.AddOptions(list);
+            //
+            list.Clear();
+            foreach (string s in headGear)
+            {
+                TMP_Dropdown.OptionData d = new TMP_Dropdown.OptionData();
+                d.text = s;
+                list.Add(d);
+                
+            }
+            headArmor.AddOptions(list);
+            //
+            list.Clear();
+            foreach (string s in armGear)
+            {
+                TMP_Dropdown.OptionData d = new TMP_Dropdown.OptionData();
+                d.text = s;
+                list.Add(d);
+                
+            }
+            armArmor.AddOptions(list);
+            //
+            list.Clear();
+            foreach (string s in chestGear)
+            {
+                TMP_Dropdown.OptionData d = new TMP_Dropdown.OptionData();
+                d.text = s;
+                list.Add(d);
+                
+            }
+            chestArmor.AddOptions(list);
+            //
+            list.Clear();
+            foreach (string s in legGear)
+            {
+                TMP_Dropdown.OptionData d = new TMP_Dropdown.OptionData();
+                d.text = s;
+                list.Add(d);
+                
+            }
+            legArmor.AddOptions(list);
+            //
+            list.Clear();
+            foreach (string s in feetGear)
+            {
+                TMP_Dropdown.OptionData d = new TMP_Dropdown.OptionData();
+                d.text = s;
+                list.Add(d);
+                
+            }
+            feetArmor.AddOptions(list);
+            //
+
+            //select first thing in each dropdown
+            selectWeapon1();
+            selectWeapon2();
             personId++;
             }
     }
