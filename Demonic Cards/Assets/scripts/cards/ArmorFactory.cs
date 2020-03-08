f6fef0a7d4f45b00b25307ce7c02409219de0e79
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-03-08 18:50:33 -0400

    Update ArmorFactory.cs

--- Demonic Cards/Assets/scripts/cards/ArmorFactory.cs
+++ Demonic Cards/Assets/scripts/cards/ArmorFactory.cs
@@ -14,6 +14,8 @@ public class ArmorFactory
         switch (str){
             case "Mining Helmet":
             return getMiningHelmet();
+            case "Brain Bucket":
+            return getBrainBucket();
         }
         throw new GenericRuntimeException("Armor not found!");
     }
@@ -24,6 +26,15 @@ public class ArmorFactory
         if (mine_helm == null){
             mine_helm = new Armor(null,"Mining Helmet","Has a bright flash.",ItemSubType.HEAD_ARMOR,new ResistMap(),new MineHelmStun(),new MineHelmStun());
             mine_helm.resist.addResist(DamageType.BLUNT,.1f);
+        }
+        return mine_helm;
+    }
+    public static Armor getBrainBucket(){
+        if (mine_helm == null){
+            mine_helm = new Armor(null,"Brain Bucket","Keeps your head on.",ItemSubType.HEAD_ARMOR,new ResistMap(),new HunkerDown(),new HunkerDown());
+            mine_helm.resist.addResist(DamageType.BLUNT,.05f);
+            mine_helm.resist.addResist(DamageType.PIERCE,.15f);
+            mine_helm.resist.addResist(DamageType.SLASH,.1f);
         }
         return mine_helm;
     }
@@ -79,6 +90,39 @@ public class MineHelmStun : Card
 
     public override float getFitness(Being user, Tile target)
     {
-        return target.occupant.stunAmount(20f);
+        return target.occupant.stunAmount(20f)/2;
+    }
+}
+
+public class HunkerDown : Card
+{
+    public HunkerDown(){
+        cName = "Hunker Down";
+        aiDisabled = true;
+        tarhint = TargetHint.ALLY;
+    }
+
+    public override float getFitness(Being user, Tile target)
+    {
+        return .5f;
+    }
+
+    public override TileSet getTileSet()
+    {
+        TileSet t = new TileSet();
+        t.tos.Add(new TileOffset(0,0));
+        return t;
+    }
+
+
+    public override void use(Being user, Tile target)
+    {
+        Buff b = new Buff();
+        b.rMap.addResist(DamageType.BLUNT,.1f);
+        b.rMap.addResist(DamageType.PIERCE,.1f);
+        b.rMap.addResist(DamageType.SLASH,.1f);
+        b.rMap.addResist(DamageType.REND,.05f);
+        b.setTimeLeft(200);
+        user.buffs.Add(b);
     }
 }
