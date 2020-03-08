8e214b618f30346e8e2b38f0cd0c7dfac768a074
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-03-08 15:17:52 -0400

    selection

---
+++ Demonic Cards/Assets/scripts/selector/select_brain.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class select_brain : MonoBehaviour
+{
+    public TMP_Dropdown weaponOne, weaponTwo, headArmor,chestArmor,armArmor,legArmor,feetArmor,classHolder,backstoryHolder;
+    private int personId = 0;
+    private Weapon wSel1, wSel2;
+    private Armor hSel, cSel,aSel,lSel,fSel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPerson();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void nextPerson(){
+        if (personId > 0){
+            //save fields
+
+        
+        }
+        if (personId == 4){
+            SceneManager.LoadSceneAsync("SampleScene");
+        }else{
+        //load stuff into dropdowns
+        personId++;}
+    }
+}
